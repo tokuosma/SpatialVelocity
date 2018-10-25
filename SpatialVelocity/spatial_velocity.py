9607f60bb19142903264af5dca02f0879873bd6b
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2018-10-25 19:43:52 +0300

    read csv,video

---
+++ SpatialVelocity/spatial_velocity.py
@@ -0,0 +1,45 @@
+import numpy as np
+import datetime
+import cv2
+import sys
+
+option_handle_list = ['--csv', '--v', '--o']
+options = {}
+
+if __name__ == "__main__":
+
+
+    for option_handle in option_handle_list:
+        options[option_handle[2:]] = sys.argv[sys.argv.index(option_handle) + 1] if option_handle in sys.argv else None
+
+    if options['csv'] ==  None:
+        print("No csv path given")
+        exit(1)
+
+    if options['v'] ==  None:
+        print("No video path given")
+        exit(1)
+
+    if options['o'] == None:
+        print("No output path given")
+
+    data = []
+    import csv
+    with open(options['csv']) as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(5000))
+        csvfile.seek(0)
+        reader = csv.DictReader(csvfile,dialect=dialect)
+        for row in reader:
+            data.append(row)
+
+    for row in data:
+        print ("pos: %.2f,%.2f,%.2f" % (float(row['x']),float(row['y']),float(row['z'])))
+            
+    cap = cv2.VideoCapture(options['v'])
+    cap.set(cv2.CAP_PROP_POS_MSEC, 500000)
+    ret, frame = cap.read()
+    cv2.imwrite('frame.png', frame)
+    cap.release()
+    cv2.destroyAllWindows()
+
+

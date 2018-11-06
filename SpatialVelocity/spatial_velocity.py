0875d6913aa8b507d840399256f84d4e6559184c
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2018-11-06 12:54:35 +0200

    SpatialVelocityDataRow

--- SpatialVelocity/spatial_velocity.py
+++ SpatialVelocity/spatial_velocity.py
@@ -2,6 +2,7 @@ import numpy as np
 import datetime
 import cv2
 import sys
+from sv_data_row import SpatialVelocityDataRow
 
 option_handle_list = ['--csv', '--v', '--o']
 options = {}
@@ -23,20 +24,31 @@ if __name__ == "__main__":
     if options['o'] == None:
         print("No output path given")
 
-    data = []
+    raw_data = []
     import csv
     with open(options['csv']) as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(5000))
         csvfile.seek(0)
         reader = csv.DictReader(csvfile,dialect=dialect)
         for row in reader:
-            data.append(row)
+            raw_data.append(row)
 
-    for row in data:
-        timestamp = datetime.datetime.strptime(row["TIMESTAMP"], "%Y-%m-%dT%H:%M:%S.%fZ")
-        print(row)
-        print ("pos: %.2f,%.2f,%.2f" % (float(row['x']),float(row['y']),float(row['z'])))
-        print (str(timestamp),row["TIMESTAMP"] )
+    data_rows = []
+    for row in raw_data:
+        data_row = SpatialVelocityDataRow(
+            row['x'], row['y'], row['z'],
+            row['r'], row['p'], row['yw'],
+            row['u_x'], row['u_y'], row['u_z'],
+            row['f_x'], row['f_y'], row['f_z'],
+            row['FPS'],  row['TIMESTAMP']
+        )
+        print(data_row.timestamp)
+        data_rows.append([data_row])
+        
+        # timestamp = datetime.datetime.strptime(row["TIMESTAMP"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        # print(row)
+        # print ("pos: %.2f,%.2f,%.2f" % (float(row['x']),float(row['y']),float(row['z'])))
+        # print (str(timestamp),row["TIMESTAMP"] )
 
     cap = cv2.VideoCapture(options['v'])
     cap.set(cv2.CAP_PROP_POS_MSEC, 1000)
--- SpatialVelocity/sv_data_row.py
+++ SpatialVelocity/sv_data_row.py
@@ -1,6 +1,16 @@
 import numpy as np
 import datetime
 
+
+
 class SpatialVelocityDataRow:
-    def __init__(self, x,y,z, roll,pitch,yaw, u_x,u_y,u_z, f_x,f_z, fps, timestamp):
-        pass
+    def __init__(self, x,y,z, roll,pitch,yaw, u_x,u_y,u_z, f_x,f_y,f_z, fps, timestamp):
+        self.wpos:np.ndarray = np.array([float(x), float(y), float(z)])
+        self.rot:np.ndarray = np.array([float(roll), float(pitch), float(z)])
+        self.forward:np.ndarray = np.array([float(f_x), float(f_y), float(f_z)])
+        self.up:np.ndarray = np.array([float(u_x), float(u_y), float(u_z)])
+        self.right:np.ndarray = np.cross(self.forward, self.up)
+        self.fps:float = float(fps)
+        self.timestamp:datetime.datetime = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+

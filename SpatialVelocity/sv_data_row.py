065919bb681e345a317a42d4cfab3f0b280a4d8d
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2019-02-12 15:48:38 +0200

    documentation improvements

--- SpatialVelocity/sv_data_row.py
+++ SpatialVelocity/sv_data_row.py
@@ -2,6 +2,21 @@ import numpy as np
 import datetime
 
 class SpatialVelocityDataRow:
+    """ Container class for spatial velocity log data rows
+
+    Attributes:
+        wpos (:obj:`np.array`): 3x1 numpy array containing the world position
+        rot (:obj:`np.array`): 3x1 numpy array containing the camera rotation
+        forward (:obj:`np.array`): 3x1 numpy array containing the forward
+            direction of player centered coordinate system.
+        right (:obj:`np.array`): 3x1 numpy array containing the right
+            direction of player centered coordinate system.
+        up (:obj:`np.array`): 3x1 numpy array containing the upward
+            direction of player centered coordinate system.
+        fps (float): Approximate FPS at write time
+        timestamp (:obj:`datetime.datetime`): Timestamp of the log row
+        
+    """
     def __init__(self, x,y,z, roll,pitch,yaw, u_x,u_y,u_z, f_x,f_y,f_z, fps, timestamp):
         self.wpos:np.ndarray = np.array([float(x), float(y), float(z)])
         self.rot:np.ndarray = np.array([float(roll), float(pitch), float(yaw) ])
--- tests/test_spatial_velocity.py
+++ tests/test_spatial_velocity.py
@@ -3,15 +3,19 @@ import numpy as np
 import sys 
 import cv2
 
-from SpatialVelocity.spatial_velocity import calculate_row_rms
+from SpatialVelocity.spatial_velocity import calculate_col_rms
 from SpatialVelocity.spatial_velocity import get_avg_row_SF
 
 class TestSpatialVelocity(unittest.TestCase):
-    def test_calculate_row_rms(self):
+    def test_calculate_col_rms_2D(self):
         mat1 = np.array([[1,2,-5],[-1,-3,-5],[-1,-4,-5]])
-        rms = calculate_row_rms(mat1)
+        rms = calculate_col_rms(mat1)
+        self.assertEqual(rms[0], 1)
+        self.assertEqual(rms[2], 5)
 
-
+    def test_calculate_col_rms_1D(self):
+        rms = calculate_col_rms(np.array([-2, 5, -8, 9, -4]))
+        self.assertEqual(6.16, np.round(rms,decimals=2))
 
     def test_avg_row_sf_bars(self):
         """ Calculate average row and column SF's from test image.
